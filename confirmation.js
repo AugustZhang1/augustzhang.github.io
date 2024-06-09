9f5297fe569551a373aff15bf4dacbb518ab451c
August Zhang <azhan157@uottawa.ca>
2024-06-09 16:37:01 -0400

    FINAL PUSH!!

--- confirmation.js
+++ confirmation.js
@@ -1,6 +1,7 @@
 document.addEventListener("load", function() {
     const form = document.querySelector('.appointment-section');
     const serviceInput = document.getElementById('dental-service');
+    const nummberOfPeopleInput = document.getElementById('number-of-people');
     const meetingStyleInput = document.getElementById('meeting-style');
     const nameInput = document.getElementById('name');
     const emailInput = document.getElementById('email');
